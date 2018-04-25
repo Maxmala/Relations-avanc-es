2c02526909918bf2b74ca929cc20ad988984b7e0
Maxmala <37975336+Maxmala@users.noreply.github.com>
2018-04-25 18:35:07 +0200

    Add files via upload

---
+++ doctolib_ok/db/migrate/20180425134002_add_patient_doctor_to_appointments.rb
@@ -0,0 +1,6 @@
+class AddPatientDoctorToAppointments < ActiveRecord::Migration[5.1]
+  def change
+  	add_reference :appointments, :patient, foreign_key: true
+  	add_reference :appointments, :doctor, foreign_key: true
+  end
+end
